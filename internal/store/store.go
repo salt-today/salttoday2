ec6d1e57e4efb8517277017cf6d89b7d73b6b190
William Sigouin <willsigouin@gmail.com>
2023-03-02 23:38:58 -0600

    use my new fancy sql package and test it out

--- internal/store/store.go
+++ internal/store/store.go
@@ -2,7 +2,10 @@ package store
 
 import (
 	"database/sql"
+	"github.com/doug-martin/goqu/v9"
+	_ "github.com/doug-martin/goqu/v9/dialect/mysql"
 	_ "github.com/go-sql-driver/mysql"
+	"time"
 )
 
 type storage struct {
@@ -10,7 +13,7 @@ type storage struct {
 }
 
 func NewStorage() (*storage, error) {
-	db, err := sql.Open("mysql", "salt:salt@tcp(localhost:3306)/salt")
+	db, err := sql.Open("mysql", "salt:salt@tcp(localhost:3306)/salt?parseTime=true")
 	if err != nil {
 		return nil, err
 	}
@@ -20,15 +23,86 @@ func NewStorage() (*storage, error) {
 	}, nil
 }
 
-func (s *storage) AddComments(comments []*Comment) error {
-	// TODO: batch me
+func (s *storage) AddComments(comments ...*Comment) error {
+	ds := goqu.Dialect("mysql").Insert(CommentsTable).Cols(CommentsUser, CommentsTime, CommentsText, CommentsLikes, CommentsDislikes)
 	for _, comment := range comments {
-		_, err := s.db.Exec(InsertComment, comment.User, comment.Time, comment.Text, comment.Likes, comment.Dislikes)
-		if err != nil {
-			return err
-		}
+		ds = ds.Vals(goqu.Vals{comment.User, comment.Time, comment.Text, comment.Likes, comment.Dislikes})
 	}
-	return nil
+	query, _, err := ds.ToSQL()
+	if err != nil {
+		return err
+	}
+
+	_, err = s.db.Exec(query)
+	return err
+}
+
+func (s *storage) GetUserComments(user string) ([]*Comment, error) {
+	sd := goqu.Dialect("mysql").
+		Select(CommentsUser, CommentsTime, CommentsText, CommentsLikes, CommentsDislikes).
+		From(CommentsTable).
+		Where(goqu.Ex{CommentsUser: user})
+
+	query, _, err := sd.ToSQL()
+	if err != nil {
+		return nil, err
+	}
+
+	rows, err := s.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	comments := make([]*Comment, 0)
+	var foundUser string
+	var tm time.Time
+	var text string
+	var likes int32
+	var dislikes int32
+	for rows.Next() {
+		err := rows.Scan(&foundUser, &tm, &text, &likes, &dislikes)
+		if err != nil {
+			return nil, err
+		}
+
+		comments = append(comments, &Comment{
+			User:     foundUser,
+			Time:     tm.Local(),
+			Text:     text,
+			Likes:    likes,
+			Dislikes: dislikes,
+		})
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	if err = rows.Close(); err != nil {
+		return nil, err
+	}
+
+	return comments, nil
+}
+
+func (s *storage) AddArticles(articles ...*Article) error {
+	ds := goqu.Insert(ArticlesTable).Cols(ArticlesUrl, ArticlesTitle)
+	for _, article := range articles {
+		ds = ds.Vals(goqu.Vals{article.Url, article.Title})
+	}
+	_, err := s.db.Exec(ds.ToSQL())
+	return err
+}
+
+func (s *storage) AddUsers(users ...string) error {
+	ds := goqu.Insert(UsersTable).Cols(UsersUser)
+	for _, user := range users {
+		ds = ds.Vals(goqu.Vals{user})
+	}
+
+	_, err := s.db.Exec(ds.ToSQL())
+	return err
 }
 
 func (s *storage) Shutdown() error {
--- internal/store/store_test.go
+++ internal/store/store_test.go
@@ -2,12 +2,16 @@ package store
 
 import (
 	"github.com/stretchr/testify/require"
+	"math/rand"
 	"testing"
+	"time"
 )
 
-// This test will fuck up your local Comments table if it fails and junk. Just here to smoke test.
+// These tests will fuck up your local Comments table if it fails and junk. Just here to smoke test.
+// Good to validate our db service is working, but not much else.
+// Should probably also mock the DB, but meh.
 // Will remove, probably.
-func TestNewStorage(t *testing.T) {
+func TestBasicDB(t *testing.T) {
 	store, err := NewStorage()
 	require.NoError(t, err)
 
@@ -41,4 +45,48 @@ func TestNewStorage(t *testing.T) {
 	require.NoError(t, err)
 
 	require.NoError(t, store.Shutdown())
+}
+
+func TestStorage_Comments(t *testing.T) {
+	store, err := NewStorage()
+	require.NoError(t, err)
+
+	_, err = store.db.Exec("TRUNCATE TABLE Comments;")
+	require.NoError(t, err)
+
+	users := []string{"userA", "userB", "userC", "userD", "userE"}
+	texts := []string{
+		"I think this is cool",
+		"i think this is dumb",
+		"i think you're all dumb",
+		"we should protest this",
+		"they should all be fired",
+	}
+
+	allComments := make([]*Comment, 0, 100)
+	commentsByUser := make(map[string][]*Comment)
+	for i := 0; i < 100; i++ {
+		user := users[i%len(users)]
+		comment := &Comment{
+			User:     user,
+			Time:     time.Now().Truncate(time.Second),
+			Text:     texts[i%len(texts)],
+			Likes:    rand.Int31(),
+			Dislikes: rand.Int31(),
+		}
+
+		allComments = append(allComments, comment)
+
+		commentsByUser[user] = append(commentsByUser[user], comment)
+	}
+
+	require.NoError(t, store.AddComments(allComments...))
+	for _, user := range users {
+		comments, err := store.GetUserComments(user)
+		require.NoError(t, err)
+		require.ElementsMatch(t, commentsByUser[user], comments)
+	}
+
+	_, err = store.db.Exec("TRUNCATE TABLE Comments;")
+	require.NoError(t, err)
 }
