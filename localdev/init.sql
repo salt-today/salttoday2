10d257524361aebd25495dde6cfc585dd08618ee
William Sigouin <willsigouin@gmail.com>
2023-03-02 22:15:33 -0600

    add basic docker compose

---
+++ localdev/init.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE IF NOT EXISTS salt;
+USE salt;
+CREATE TABLE IF NOT EXISTS Comments (
+    User VARCHAR(255) NOT NULL,
+    Time DATETIME NOT NULL,
+    Text MEDIUMTEXT NOT NULL,
+    Likes INT NOT NULL DEFAULT(0),
+    Dislikes INT NOT NULL DEFAULT(0),
+    INDEX "user" (User)
+);
+
+CREATE TABLE IF NOT EXISTS Article (
+    Url VARCHAR(2048) NOT NULL,
+    Title VARCHAR(2048) NOT NULL,
+    PRIMARY KEY (Url)
+);
+
+Create TABLE IF NOT EXISTS Users (
+    #hmm what else to store here? Do I need it?
+    User VARCHAR(255) NOT NULL,
+    PRIMARY KEY (User)
+);
+
+FLUSH PRIVILEGES;
+SELECT * FROM User;
+CREATE USER 'salt'@'%' IDENTIFIED BY 'salt';
+GRANT ALL PRIVILEGES ON *.* TO 'salt'@'%';
+FLUSH PRIVILEGES;
