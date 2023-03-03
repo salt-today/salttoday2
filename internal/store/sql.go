e7ded90b7d143d29c7738f29c61996a4a7ccb01f
William Sigouin <willsigouin@gmail.com>
2023-03-02 23:37:50 -0600

    define db consts

---
+++ internal/store/sql.go
@@ -0,0 +1,22 @@
+package store
+
+// Tables
+const (
+	CommentsTable = "Comments"
+	UsersTable    = "Users"
+	ArticlesTable = "Articles"
+)
+
+// Columns
+const (
+	CommentsUser     = "User"
+	CommentsTime     = "Time"
+	CommentsText     = "Text"
+	CommentsLikes    = "Likes"
+	CommentsDislikes = "Dislikes"
+
+	ArticlesUrl   = "Url"
+	ArticlesTitle = "Title"
+
+	UsersUser = "User"
+)
