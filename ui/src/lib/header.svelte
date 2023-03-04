eb66c0289b793ceb437794d26368517160e571b1
Claire Knutson <claire.e.knutson@gmail.com>
2023-03-03 22:18:34 -0700

    add winner card

---
+++ ui/src/lib/header.svelte
@@ -0,0 +1,34 @@
+<script>
+    import {
+		Navbar,
+		NavBrand,
+		NavHamburger,
+		Avatar,
+		Dropdown,
+		DropdownItem,
+		DropdownHeader,
+		DropdownDivider,
+	} from 'flowbite-svelte';
+</script>
+<Navbar let:hidden let:toggle color="red">
+	<NavBrand href="/">
+		<img src="salt-shaker.png" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+			>Salt Mines</span
+		>
+	</NavBrand>
+	<div class="flex items-center md:order-2">
+		<Avatar id="avatar-menu">SM</Avatar>
+		<NavHamburger on:click={toggle} class1="w-full md:flex md:w-auto md:order-1" />
+	</div>
+	<Dropdown placement="bottom" triggeredBy="#avatar-menu">
+		<DropdownHeader>
+			<span class="block text-sm"> Salty McSalterton </span>
+			<span class="block truncate text-sm font-medium"> mcsalty@gmail.com </span>
+		</DropdownHeader>
+		<DropdownItem>Leaderboard</DropdownItem>
+		<DropdownItem>Profile</DropdownItem>
+		<DropdownDivider />
+		<DropdownItem>Sign out</DropdownItem>
+	</Dropdown>
+</Navbar>
--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,27 +1,20 @@
 <script>
-	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Avatar, Dropdown, DropdownItem, DropdownHeader, DropdownDivider } from 'flowbite-svelte'
+	import Header from '../lib/header.svelte';
+	import WinnerCard from '../lib/winner-card.svelte';
 </script>
 
-<Navbar let:hidden let:toggle color="blue">
-    <NavBrand href="/">
-      <img src="salt-shaker.png" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo"/>
-      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Salt Mines</span>
-    </NavBrand>
-    <div class="flex items-center md:order-2">
-      <Avatar id="avatar-menu">SM</Avatar>
-      <NavHamburger on:click={toggle} class1="w-full md:flex md:w-auto md:order-1"/>
-    </div>
-    <Dropdown placement="bottom" triggeredBy="#avatar-menu">
-      <DropdownHeader>
-      <span class="block text-sm"> Bonnie Green </span>
-      <span class="block truncate text-sm font-medium"> name@flowbite.com </span>
-      </DropdownHeader>
-      <DropdownItem>Leaderboard</DropdownItem>
-      <DropdownItem>Profile</DropdownItem>
-      <DropdownDivider />
-      <DropdownItem>Sign out</DropdownItem>
-    </Dropdown>
-  </Navbar>
-
-<h1>Leaderboard</h1>
+<Header />
+<div class="container">
+	<div class="flex mt-10">
+		<div class="basis-1/3">
+			<WinnerCard title="3rd Place" initials="SP" name="Sad Panda" />
+		</div>
+		<div class="basis-1/3">
+			<WinnerCard title="1st Place" initials="CK" name="Captain Kangaroo" />
+		</div>
+		<div class="basis-1/3">
+			<WinnerCard title="2nd Place" initials="HK" name="Hello Kitty" />
+		</div>
+	</div>
+</div>
 
