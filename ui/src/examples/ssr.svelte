bc4df86578cae1c17785c52a5a7c723909a673fe
Hunter Breathat <nexushunterdev@gmail.com>
2023-03-05 00:08:38 -0500

    Comments & Filter menu

--- ui/src/examples/ssr.svelte
+++ ui/src/examples/ssr.svelte
@@ -1,54 +1,90 @@
 <script lang="ts">
- import {onMount} from 'svelte';
- import axios from 'axios';
- import qs from 'qs';
+	import { onMount } from 'svelte';
+	import axios from 'axios';
+	import qs from 'qs';
 
-type Comment = {
-    author:string;
-    content:string;
-    datePosted: Date;
-}
+	// Defines a comment returned by the scraper.
+	type Comment = {
+		author: string;
+		content: string;
+		datePosted: Date;
+	};
 
-type Filters = {
-    since?: Date;
-    author?: string;
-    liked?: boolean;
-    disliked?: boolean;
-    deleted?: boolean;
-}
+	// Filter comments based on the following traits.
+	type Filters = {
+		since?: Date;
+		author?: string;
+		liked?: boolean;
+		disliked?: boolean;
+		deleted?: boolean;
+	};
 
- const ITEMS_PER_PAGE = 10;
- let currentPage = 1;
- let totalComments = 0;
- let comments: Comment[] = [];
- let currentFilters:Filters = {};
+    let includeUserName = false;
+    let authorNames = '';
 
- async function fetchComments({page = 1, filters = currentFilters}) {
-    const {since, ...rest} = filters
-    const queryParams = qs.stringify({page, ITEMS_PER_PAGE, since: since?.getUTCMilliseconds, ...rest})
-    // FIXME: use corrected endpoint
-    const response = await axios.get(`/api/v1/comments?${queryParams}`)
-    currentPage = page;
-    totalComments = response.data.totalComments;
-    comments = response.data.comments.map((c: any) => ({author:c.author, content: c.content, datePosted: Date.parse(c.datePosted)}));
- }
- onMount(()=>fetchComments({page: 1}))
+	const ITEMS_PER_PAGE = 10;
+	let currentPage = 1;
+	let totalComments = 0;
+	let comments: Comment[] = [];
+	let currentFilters: Filters = {};
+
+    // Fetch the comments from the scraper by the page & ITEMS_PER_PAGE. Also pass along any filtering provided by the user in the request.
+	async function fetchComments({ page = 1, filters = currentFilters }) {
+		const { since, ...rest } = filters;
+		const queryParams = qs.stringify({
+			page,
+			ITEMS_PER_PAGE,
+			since: since?.getUTCMilliseconds,
+			...rest
+		});
+		// FIXME: use corrected endpoint
+		const response = await axios.get(`/api/v1/comments?${queryParams}`);
+		currentPage = page;
+		totalComments = response.data.totalComments;
+		comments = response.data.comments.map((c: any) => ({
+			author: c.author,
+			content: c.content,
+			datePosted: Date.parse(c.datePosted)
+		}));
+	}
+	onMount(() => fetchComments({ page: 1 }));
 </script>
 
 <div>
+	<ul>
+		{#each comments as comment}
+			<li>{comment.author}</li>
+		{/each}
+	</ul>
+
     <ul>
-        {#each comments as comment}
-            <li>{comment.author}</li>
-        {/each}
+        <button on:click={() => currentFilters.liked = true} >Liked</button>
+        <button on:click={() => currentFilters.disliked = true} >Disliked</button>
+        <form id={'name-input-form'} on:submit={() => {
+            if (includeUserName) {
+                currentFilters.author=authorNames;
+            }
+        }}>
+            <checkbox checked={includeUserName} on:change={() => includeUserName = !includeUserName}>
+                <label for={'name-input'}>Username:</label>
+            <input required={includeUserName} on:change={(v: HTMLInputElement) => authorNames = v.target.value} type={'text'} id={'name-input'} name={'name-input-form'} maxlength={32} size={48}>
+            </checkbox>
+        </form>
+
+        <!-- FIXME: ADD Date picker-->
     </ul>
 
-    <nav>
-        {#if currentPage > 1}
-            <button on:click={() => fetchComments({page: currentPage -1, filters: currentFilters})}>Previous</button>
-        {/if}
+	<nav>
+		{#if currentPage > 1}
+			<button on:click={() => fetchComments({ page: currentPage - 1, filters: currentFilters })}
+				>Previous</button
+			>
+		{/if}
 
-        {#if currentPage < Math.ceil(totalComments / ITEMS_PER_PAGE)}
-            <button on:click={() => fetchComments({page: currentPage -1, filters: currentFilters})}>Next</button>
-        {/if}
-    </nav>
+		{#if currentPage < Math.ceil(totalComments / ITEMS_PER_PAGE)}
+			<button on:click={() => fetchComments({ page: currentPage - 1, filters: currentFilters })}
+				>Next</button
+			>
+		{/if}
+	</nav>
 </div>
