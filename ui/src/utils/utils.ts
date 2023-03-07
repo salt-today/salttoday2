6dc32b3134c5659276eb2cc6e939f9e2f5ee111d
Hunter Breathat <nexushunterdev@gmail.com>
2023-03-06 21:30:47 -0500

    Add dependabot & comment

--- ui/src/utils/utils.ts
+++ ui/src/utils/utils.ts
@@ -1,4 +1,6 @@
+// Parse the object provided into Query Parameters.
 export default function parseQueryParams(params: any): string {
+	// TODO: URL SafeEncode this?
 	return Object.keys(params)
 		.map((k) => `${params[k] === undefined || params[k] === null ? '' : `${k}=${params[k]}`}`)
 		.join('&');
