efec70bd1ebac244ba23aaab2380c1bccaa0256e
Dan Harasymiw <danharasymiw@gmail.com>
2024-04-28 20:31:39 -0400

    SMALLER

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,7 @@ module.exports = {
   content: ["internal/server/ui/**/*.templ"],
   // Can't have dynamic values via string interpolation
   // Need this safelist for gradients on comments to work
-  safelist: Array.from(new Array(20), (_, index) => 'to-' + index * 5 + '%').concat(
-    Array.from(new Array(20), (_, index) => 'from-' + index * 5 + '%')).concat(
-      Array.from(new Array(101), (_, index) => 'w-[' + index + '%]')
-    ),
+  safelist: Array.from(new Array(20), (_, i) => [`to-${i * 5}%`, `from-${i * 5}%`]).flat().concat(Array.from(new Array(101), (_, i) => `w-${i}%`)),
   theme: {
     fontFamily: {
       bebas: ['Bebas Neue', 'sans-serif'],
